40eac2022548b19b0f9925b60c983dc05dea333e
ParkJongSeong93 <97346583+ParkJongSeong93@users.noreply.github.com>
2023-10-28 13:44:40 +0900

    YS checked(src/app -> src)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Footer from "./components/footer/Footer";
-import Mainbody from "./components/mainbody/Mainbody";
-import Mainnav from "./components/mainnav/Mainnav";
+import Footer from "../components/footer/Footer";
+import Mainbody from "../components/mainbody/Mainbody";
+import Mainnav from "../components/mainnav/Mainnav";
 
 export default function Home() {
   return (
@@ -11,3 +11,6 @@ export default function Home() {
     </main>
   );
 }
+
+//폴더 구조에 기준을 두고 나중에 많은 디렉토리가 생겼을 때를 대비 필
+//folder struct depth를 1차원으로 고정하지 않아도 됨
