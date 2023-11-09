7f4e3beec8c73476e9058a8672d80ee7016e8486
ParkJongSeong93 <97346583+ParkJongSeong93@users.noreply.github.com>
2023-11-09 16:20:18 +0900

    feat: feedback페이지 퍼블리싱(비디오, 스크립트), Sidebar component를 feedback폴더로 생성

--- src/app/feedback/[id]/page.tsx
+++ src/app/feedback/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Mainnav from "@/components/common/Mainnav";
+import Sidebar from "@/components/feedback/Sidebar";
+import Footer from "@/components/common/Footer";
 
 type Props = {
   params: {
@@ -27,9 +30,46 @@ function Page({ params }: Props) {
   }, []);
 
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-      {params.id}번 동영상 피드백 상세 페이지
-    </div>
+    <>
+      <div className="w-full h-screen flex flex-col">
+        <Mainnav />
+        <div className="flex w-full flex-1">
+          <Sidebar />
+          <div className="flex-1 flex flex-col justify-center items-center">
+            <div className="flex w-5/6 h-1/3">
+              <div className="flex flex-col w-2/5 mr-2.5">
+                <p className="font-bold text-2xl mb-2">Test video {params.id}</p>
+                <iframe
+                  src="https://www.youtube.com/embed/3mPPMCa8Nxw"
+                  title="Top Finishes: Conor McGregor"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                  className="h-full w-full"
+                ></iframe>
+              </div>
+              <div className="flex flex-col w-3/5">
+                <p className="font-bold text-2xl mb-2">Transcript</p>
+                <div className="w-full h-1 bg-gray-300"></div>
+                <div className="w-full h-full my-2 p-1 bg-gray-100 font-light text-xl">test {params.id}</div>
+                <div className="w-full h-1 bg-gray-300"></div>
+              </div>
+            </div>
+            <div className="flex-col w-5/6 mt-16">
+              <div className="w-full py-3 mb-3 bg-gray-200 text-center">
+                A
+              </div>
+              <div className="w-full py-3 mb-3 bg-gray-200 text-center">
+                B
+              </div>
+              <div className="w-full py-3 mb-3 bg-gray-200 text-center">
+                C
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/feedback/Sidebar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Video } from "@/types/video";
+
+function Sidebar() {
+  const [videoList, setVideoList] = useState<Video[]>([
+    {
+      id: 1,
+      title: "Test Video",
+      videoUrl: "https://www.youtube.com",
+    },
+    {
+      id: 2,
+      title: "Test Video 2",
+      videoUrl: "https://www.youtube.com",
+    },
+  ]);
+
+  useEffect(() => {
+    const getVideoList = async () => {
+      const url = `http://localhost:8080/`;
+      const response = await fetch(url + `api/videos`);
+      const data = await response.json();
+      setVideoList(data);
+    };
+    getVideoList();
+  }, []);
+
+  return (
+    <div className="bg-uploadBg-gray flex flex-col justify-center w-1/4">
+      <Link href="/upload" className="py-4 pl-3 mb-10 bg-uploadPageBtn-gray">
+        Upload New Video
+      </Link>
+      {videoList.map((video) => (
+        <Link
+          href={`/feedback/${video.id}`}
+          className="py-4 pl-3 mb-6 bg-uploadPageBtn-lightgray text-gray-400"
+        >
+          {video.title}
+        </Link>
+      ))}
+      <div className="py-4 pl-3 mt-4 bg-uploadPageBtn-lightgray">Option</div>
+    </div>
+  );
+}
+
+export default Sidebar;
